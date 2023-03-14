ad0723b37d0f3e68931c24f1aa1359e1907ad294
darac001 <drxmrx861@gmail.com>
2023-03-14 11:57:19 -0600

    LOGO

--- src/components/MyCarousel.js
+++ src/components/MyCarousel.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import main3 from "../images/main-3.jpg";
+import main3 from "../images/main-4.jpg";
 import main from "../images/main.jpg";
 function MyCarousel() {
   const [index, setIndex] = useState(0);
@@ -10,14 +10,15 @@ function MyCarousel() {
   };
   return (
     <section className="car-hero">
-      <Carousel className="car" activeIndex={index} onSelect={handleSelect}>
+      <Carousel autoPlay={true}
+        interval={10000} className="car" activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item className="car-item">
           <img className="d-block w-100 car-img" src={main} alt="First slide" />
           <Carousel.Caption className="car-banner">
-            <h1>Essence of Saigon</h1>
             <p>
-            Discover Saigon with our exciting and immersive tours
+              Off The Beaten Path Adventures
             </p>
+            <h1>Essence of Saigon</h1>
             <a href="#tours" className="btn">
               explore tours
             </a>
@@ -30,10 +31,8 @@ function MyCarousel() {
             alt="First slide"
           />
           <Carousel.Caption className="car-banner">
+
             <h1>food scene</h1>
-            <p>
-            Indulge in the rich and diverse flavors of Saigon's food scene with our mouthwatering tours.
-            </p>
             <a href="#tours" className="btn hero-btn">
               check it out
             </a>
